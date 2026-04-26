53cbedfcd7387107f413043c6f12e139ef743a18
Mainak Sen <mainak1331sen@gmail.com>
2026-04-26 13:04:40 +0530

    Version 2 pushed

--- run.bat
+++ run.bat
@@ -1,15 +1,20 @@
-cls
-echo Loading Mini Compiler...
-timeout /t 1 >nul
-cls
-
 @echo off
-cls
 color 0A
 title Mini Compiler
 
+:: ---------- Splash ----------
+cls
 echo =====================================
-echo        MINI COMPILER
+echo        MINI COMPILER 🚀
+echo =====================================
+echo.
+echo Initializing...
+timeout /t 1 >nul
+
+:: ---------- Build ----------
+cls
+echo =====================================
+echo        BUILDING PROJECT
 echo =====================================
 echo.
 
@@ -17,20 +22,29 @@ echo [1/4] Cleaning...
 del *.exe *.tab.c *.tab.h lex.yy.c >nul 2>&1
 
 echo [2/4] Running Flex...
-win_flex lexer.l >nul
+win_flex lexer.l >nul 2>&1
 
 echo [3/4] Running Bison...
-win_bison -d parser.y >nul
+win_bison -d parser.y >nul 2>&1
 
 echo [4/4] Compiling...
-gcc lex.yy.c parser.tab.c ast.c symtab.c ir.c opt.c codegen.c main.c -o compiler.exe >nul
+gcc lex.yy.c parser.tab.c ast.c symtab.c ir.c opt.c codegen.c main.c -o compiler.exe >nul 2>&1
 
 echo.
-echo ===== RUNNING PROGRAM =====
+echo ✅ Build Successful!
+timeout /t 1 >nul
+
+:: ---------- Run ----------
+cls
+echo =====================================
+echo        PROGRAM OUTPUT
+echo =====================================
 echo.
 
 compiler.exe < input.c
 
 echo.
-echo ===== DONE =====
+echo =====================================
+echo              DONE
+echo =====================================
 pause
